3ee2324dac52e6a086941b838a81465a7d4cddf0
ezegavilan <ezegavilan.developer@gmail.com>
2022-04-01 09:40:20 -0300

    Primer commit

---
+++ src/app/components/histograma/histograma.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-histograma',
+  templateUrl: './histograma.component.html',
+  styleUrls: ['./histograma.component.css']
+})
+export class HistogramaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
